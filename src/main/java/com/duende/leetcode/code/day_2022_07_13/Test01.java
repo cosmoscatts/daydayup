d3706ed2582945765fbc4447b391b1fb9d5097bb
faitsse <2138889191@qq.com>
2022-07-13 20:28:54 +0800

    chore: init

---
+++ src/main/java/com/duende/leetcode/code/day_2022_07_13/Test01.java
@@ -0,0 +1,12 @@
+package com.duende.leetcode.code.day_2022_07_13;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Test01 {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        return null;
+    }
+}
