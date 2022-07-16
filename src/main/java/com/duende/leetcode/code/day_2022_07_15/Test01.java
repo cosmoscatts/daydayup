357080f5ba849fe8080f3f41e40797a0544ef6ab
faitsse <2138889191@qq.com>
2022-07-16 08:09:04 +0800

    feat: update docs

--- src/main/java/com/duende/leetcode/code/day_2022_07_15/Test01.java
+++ src/main/java/com/duende/leetcode/code/day_2022_07_15/Test01.java
@@ -1,4 +1,29 @@
 package com.duende.leetcode.code.day_2022_07_15;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Test01 {
+
+}
+
+class MovingAverage {
+    Queue<Integer> queue;
+    int size;
+    double sum;
+
+    public MovingAverage(int size) {
+        queue = new ArrayDeque<Integer>();
+        this.size = size;
+        sum = 0;
+    }
+
+    public double next(int val) {
+        if (queue.size() == size) {
+            sum -= queue.poll();
+        }
+        queue.offer(val);
+        sum += val;
+        return sum / queue.size();
+    }
 }
