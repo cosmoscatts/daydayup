8658bdf29d30e53cb1d796b7a4ce08ee6f6c700c
faitsse <2138889191@qq.com>
2022-08-19 19:10:44 +0800

    style: format

--- src/main/java/com/duende/leetcode/code/day_2022_07_13/Test01.java
+++ src/main/java/com/duende/leetcode/code/day_2022_07_13/Test01.java
@@ -7,18 +7,22 @@ public class Test01 {
     // 7/13 日打卡
     public int[] asteroidCollision(int[] asteroids) {
         Deque<Integer> dq = new ArrayDeque<>();
-        for (int t: asteroids) {
+        for (int t : asteroids) {
             boolean ok = true;
             while (ok && !dq.isEmpty() && dq.peekLast() > 0 && t < 0) {
                 int a = dq.peekLast(), b = -t;
-                if (a <= b) dq.pollLast();
-                if (a >= b) ok = false;
+                if (a <= b)
+                    dq.pollLast();
+                if (a >= b)
+                    ok = false;
             }
-            if (ok) dq.addLast(t);
+            if (ok)
+                dq.addLast(t);
         }
         int size = dq.size();
         int[] ans = new int[size];
-        while (!dq.isEmpty()) ans[--size] = dq.pollLast();
+        while (!dq.isEmpty())
+            ans[--size] = dq.pollLast();
         return ans;
     }
 }
